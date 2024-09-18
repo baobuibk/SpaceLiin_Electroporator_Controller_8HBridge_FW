6aac2f76ceeced4e1d76b4bf4d0fae40bbed6b54
HalmondD <duonganhkhoa2002@gmail.com>
2024-09-18 12:08:57 +0700

    update bin

--- Application/app.c
+++ Application/app.c
@@ -10,28 +10,28 @@ tSchedulerTask 	g_psSchedulerTable[SCHEDULER_TASK_COUNT] =
                     {
                             &ADC_Task,
                             (void *) 0,
-                            62,                         //call every 248us
+                            3,                         //call every 248us
                             0,			                //count from start
                             true		                //is active
                     },
                     {
                             &PID_Task,
                             (void *) 0,
-                            125,                        //call every 500us
+                            5,                        //call every 500us
                             0,			                //count from start
                             true		                //is active
                     },
                     {
                             &Discharge_Task,
                             (void *) 0,
-                            125,                        //call every 500us
+                            5,                        //call every 500us
                             0,			                //count from start
                             true		                //is active
                     },
                     {
                             &CMD_Line_Task,
                             (void *) 0,
-                            125,                        //call every 500us
+                            5,                        //call every 500us
                             0,                          //count from start
                             true                        //is active
                     },
@@ -41,7 +41,7 @@ void App_Main(void)
 {   
     // STM32F030CCT6 @ 36MHz, 
     // can run scheduler tick max @ 4us.
-    SchedulerInit(250000);
+    SchedulerInit(10000);
 
     ADC_Task_Init(LL_ADC_SAMPLINGTIME_7CYCLES_5);
     PID_Task_Init();
