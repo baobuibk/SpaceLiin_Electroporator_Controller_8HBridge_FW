b4d267fa17109c91a1f362787f42cd3b0f571b34
HalmondD <duonganhkhoa2002@gmail.com>
2024-09-20 15:26:01 +0700

    commit to change branch

--- Application/app.c
+++ Application/app.c
@@ -4,43 +4,43 @@
 //TODO: Create a system to handle hard fault or smth like that.
 
 #define         SCHEDULER_TASK_COUNT  4
-uint32_t 		g_ui32SchedulerNumTasks = SCHEDULER_TASK_COUNT;
+uint32_t        g_ui32SchedulerNumTasks = SCHEDULER_TASK_COUNT;
 tSchedulerTask 	g_psSchedulerTable[SCHEDULER_TASK_COUNT] =
                 {
                     {
                             &ADC_Task,
                             (void *) 0,
                             3,                         //call every 248us
-                            0,			                //count from start
-                            true		                //is active
+                            0,			       //count from start
+                            false		       //is active
                     },
                     {
                             &PID_Task,
                             (void *) 0,
                             5,                        //call every 500us
-                            0,			                //count from start
-                            true		                //is active
+                            0,			      //count from start
+                            false		      //is active
                     },
                     {
                             &Discharge_Task,
                             (void *) 0,
                             5,                        //call every 500us
-                            0,			                //count from start
-                            true		                //is active
+                            0,			      //count from start
+                            false		      //is active
                     },
                     {
                             &CMD_Line_Task,
                             (void *) 0,
                             5,                        //call every 500us
-                            0,                          //count from start
-                            true                        //is active
+                            0,                        //count from start
+                            true                      //is active
                     },
                 };
 
 void App_Main(void)
 {   
     // STM32F030CCT6 @ 36MHz, 
-    // can run scheduler tick max @ 4us.
+    // can run scheduler tick max @ 100us.
     SchedulerInit(10000);
 
     ADC_Task_Init(LL_ADC_SAMPLINGTIME_7CYCLES_5);
